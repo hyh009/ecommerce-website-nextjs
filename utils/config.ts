1e22a3cd8c9525db98e090cfe1e52e44519ec7f2
Hsinyao <hsinyao009@gmail.com>
2022-05-13 00:08:32 +0800

    add production domain

--- utils/config.ts
+++ utils/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = process.env.NODE_ENV!=="production"? "http://localhost:3000":"";
+export const baseUrl = process.env.NODE_ENV!=="production"? "http://localhost:3000":"https://ecommerce-website-nextjs.vercel.app";
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl,

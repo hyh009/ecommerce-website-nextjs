600a8f426d2bd5bb12f2d5d643f3fb5f3da2057b
Hsinyao <hsinyao009@gmail.com>
2022-05-25 15:36:33 +0800

    change url

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// "https://ecommerce-website-nextjs.vercel.app"
 
-export const baseUrl = process.env.NODE_ENV!=="production"? "http://localhost:3000":"https://ecommerce-website-nextjs.vercel.app";
+export const baseUrl = process.env.NODE_ENV!=="production"? "http://localhost:3000":"https://hsinyaonextshop.netlify.app/";
 
 export const axiosInstance = axios.create({
     baseURL: baseUrl,
